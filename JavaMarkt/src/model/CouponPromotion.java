98a8f58fc6f5ca8ece0c92c2b6eb88ea27af9d7e
wdobkowicz <s6355@pjwstk.edu.pl>
2018-03-17 16:46:30 +0100

    Dodanie przeliczania ceny po dodaniu CouponPromotion

--- JavaMarkt/src/model/CouponPromotion.java
+++ JavaMarkt/src/model/CouponPromotion.java
@@ -24,6 +24,15 @@ public class CouponPromotion implements IPromotion{
 	public void CalculateOffer(Koszyk cart) {
 		if (this.CanCalculate(cart)) {
 			//sprawdzić czy jest taki produkt o tej nazwie i jeśli tak, to obniżyć cenę
+			System.out.println("----------------------------------\nCalculate offer - CouponPromotion");
+			this.produkt.showDetails();
+			if(this.produkt.getName()=="Kartofle"){
+				this.produkt.setPrice(this.produkt.getPrice()*0.7);
+			}
+			System.out.println("Naliczono rabat 30%!");
+			this.produkt.showDetails();
+			cart.promotionSumOfProducts = cart.totalPriceOfProducts() - this.produkt.getPrice() * 0.3;
+			System.out.println("----------------------------------");
 		}
 		
 	}
